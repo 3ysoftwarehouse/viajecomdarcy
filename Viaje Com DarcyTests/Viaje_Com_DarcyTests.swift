9a0f88e5570842b15d4ae281b9fea6e2c7f78449
Vitor Albuquerque <vitor.rcca@gmail.com>
2016-06-22 22:58:13 -0300

    Initial Commit

---
+++ Viaje Com DarcyTests/Viaje_Com_DarcyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Viaje_Com_DarcyTests.swift
+//  Viaje Com DarcyTests
+//
+//  Created by Vitor Albuquerque on 6/22/16.
+//  Copyright © 2016 Vitor Albuquerque. All rights reserved.
+//
+
+import XCTest
+@testable import Viaje_Com_Darcy
+
+class Viaje_Com_DarcyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
